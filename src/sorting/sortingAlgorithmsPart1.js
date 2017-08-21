713d64e0000335e7a3b0a11d59dd06af2b2208ec
Justin Baize <justin.baize@gJBc897ad.local>
2017-08-21 13:17:35 -0500

    Restoring original

--- src/sorting/sortingAlgorithmsPart1.js
+++ src/sorting/sortingAlgorithmsPart1.js
@@ -2,53 +2,8 @@
 
 const swap = require('./sort-helpers').swap;
 
-// this is bubbleSort [ 3.1, 4.5, 1, 2, 5 ]
-// 239) sorts an array of positive numbers
-
-// this is bubbleSort [ -4, -9, -2, -8, -2, 0 ]
-// 240) sorts an array of negative numbers
-
-// this is bubbleSort [ -3, 4, 1, -9, 22 ]
-// 241) sorts an array of positive and negative numbers
-
-// this is bubbleSort [ 1, 2, 3, 4, 5 ]
-// 242) works with arrays that are already sorted
-
-
-/**
-  name: bubbleSort
-  descr: Sorts input array into "lowest to highest".
-         (1). Passes through each arr's element
-         (2). Compares each element against element's rightSide neighbor (nextElement).
-         (3). If element is greater than nextElement, swap positions.
-  param: arr (array of unsorted numbers)
-  return: arr (sorted from lowest to highest)
-**/
-
 function bubbleSort(arr) {
-  let justSwapped = false;
 
-  arr.forEach(function(element,index){
-    let nextElement = arr[index+1];
-
-    //if nextElement is truthy AND element is greater than nextElement
-    if ( !!nextElement && ( element > nextElement ) ) {
-      //swap element's && nextElement's relative position in arr
-      arr[index] = nextElement;
-      arr[index+1] = element;
-
-      //set flag equal to true;
-      justSwapped = true;
-    }
-  })
-
-  //if swapping ever happened, recurse bubbleSort(arr)
-  if ( justSwapped ) {
-    return bubbleSort(arr);
-  }
-
-  //if swapping never happened, arr is sorted!
-  return arr;
 }
 
 function selectionSort(arr) {
